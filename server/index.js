3b6a71c1f1cb28eaaf4dbd15fbf540a11abc871d
pratyusha2001 <pratyusharouth2001@gmail.com>
2023-05-19 12:13:32 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const mongoose = require('mongoose')
+const PhoneBook = require('./model/PhoneBook')
+
+app.use(express.json())
+app.use(cors())
+
+const PORT=8080
+const uri = 'mongodb+srv://pratyusha:Pratyusha%4001@cluster0.65skl.mongodb.net/test'
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on: ${PORT} `)
+})
+
+ mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    maxIdleTimeMS: 5000
+}).then(()=>{
+    console.log('Database connected successfully')
+}) 
+
+app.post('/add-phone', async(req,res)=>{
+    const phoneNumber = new PhoneBook(req.body)
+    try{
+        await phoneNumber.save()
+        res.status(201).json({
+            status: 'Success',
+            data : {phoneNumber}
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message: err
+        })
+    }
+})
+
+app.get('/get-phone', async(req, res)=>{
+    const phoneNumbers = await PhoneBook.find({})
+    try{
+        res.status(200).json({
+            status:'Success',
+            data: {phoneNumbers}
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message: err
+        })
+    }
+})
+
+app.patch('/update-phone/:id', async(req,res)=>{
+    const updatedPhone = await PhoneBook.findByIdAndUpdate(req.params.id, req.body,{
+      new : true,
+      runValidators : true  
+    })
+    try{
+        res.status(200).json({
+            status : 'Success',
+            data : {updatedPhone}
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+app.delete('/delete-phone/:id', async(req,res)=>{
+    await PhoneBook.findByIdAndDelete(req.params.id)
+    try{
+        res.status(204).json({
+            status : 'Success',
+            data : {}
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message: err
+        })
+    }
+})
